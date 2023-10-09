49b504ae9238e834f8a3d56defbc5a4e1f93b0e9
Anna Lee <annalee883@gmail.com>
2023-10-09 18:14:29 -0400

    minor update

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -95,7 +95,7 @@ export default {
 
 <style>
 #quiz {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c2c4e;
