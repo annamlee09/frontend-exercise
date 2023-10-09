f39c81f859540459833598c5d7b4d0314950419d
Anna Lee <annalee883@gmail.com>
2023-10-09 10:54:30 -0400

    initial quiz commit - display all questions and answers in single view

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,8 +1,24 @@
 <template>
-  <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+  <div id="quiz">
+    <h1>Cognito Forms Quiz</h1>
+
+
+    <div v-for="(question, index) in questions" :key="index">
+      <Question :question="question"></Question>
+    </div>
+    <div>
+      {{ `Question ${currentQuestion} of ${questions.length}` }}
+    </div>
+    <div v-if="showSummary">
+
+    </div>
+  </div>
 </template>
 
 <script>
+
+import Question from '@/components/Question.vue';
+
 export default {
   props: {
     questions: {
@@ -10,8 +26,27 @@ export default {
       required: true
     }
   },
+  components: {
+    Question
+  },
   data() {
-    return {};
+    return {
+      showSummary: false
+    };
+  },
+  computed: {
   }
 };
+
 </script>
+
+<style>
+
+#quiz {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c2c4e;
+  text-align: center;
+}
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="question">
+            <span>
+                {{ question.text }}
+            </span>
+        </div>
+        <form>
+            <button class="answer" v-for="(answer, index) in question.answers" :key="index">
+                {{ answer }}
+            </button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'question',
+    data() {
+        return {
+
+        };
+    },
+    props: ['question']
+}
+</script>
+
+<style scoped>
+.question {
+    font-size: 18px;
+}
+</style>
